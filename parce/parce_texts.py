1c564128c657dd96b7a4f91d9c5ca0b98dd5db88
vgarshin <vgarshin@yandex.ru>
2024-03-27 10:14:28 +0000

    app

--- parce/parce_texts.py
+++ parce/parce_texts.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 import os
 import re
 import sys
+import ssl
 import json
 import boto3
 import socket
-from random import randint, uniform
+from random import randint, uniform, choice, getrandbits
 from urllib.request import (
     Request, 
     urlopen, 
@@ -25,24 +26,26 @@ from time import sleep, gmtime, strftime
 from tqdm import tqdm
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 YaBrowser/19.6.1.153 Yowser/2.5 Safari/537.36'
-MIN_TIME_SLEEP = .5
+MIN_TIME_SLEEP = .1
 MAX_TIME_SLEEP = 10
 MAX_COUNTS = 10
 MAX_ARTS = 1000
 TIMEOUT = 30
 
 class TextCollector():
-    def __init__(self, base_url, user_agent, timeout, max_counts, 
-                 min_time_sleep, max_time_sleep, s3_access_file):
+    def __init__(self, base_url, user_agent, proxies, timeout, max_counts, 
+                 min_time_sleep, max_time_sleep, s3_access_file, browser=False):
         self.base_url = base_url
         self.user_agent = user_agent
+        self.proxies = proxies
         self.timeout = timeout
         self.max_counts = max_counts
         self.min_time_sleep = min_time_sleep
         self.max_time_sleep = max_time_sleep
-        opts = FirefoxOptions()
-        opts.add_argument('--headless')
-        self.browser = webdriver.Firefox(options=opts)
+        if browser:
+            opts = FirefoxOptions()
+            opts.add_argument('--headless')
+            self.browser = webdriver.Firefox(options=opts)
         self.s3 = boto3.client('s3')
         with open(s3_access_file) as file:
             self.access_data = json.load(file)
@@ -69,13 +72,13 @@ class TextCollector():
                 request = Request(url_page)
                 request.add_header('User-Agent', self.user_agent)
                 if proxies:
-                    proxy_support = ProxyHandler(proxies)
+                    prx = choice(proxies)
+                    proxy_support = ProxyHandler(prx)
                     opener = build_opener(proxy_support)
                     install_opener(opener)
-                    context = ssl._create_unverified_context()
                     response = urlopen(
                         request, 
-                        context=context, 
+                        context=ssl._create_unverified_context(),
                         timeout=self.timeout
                     )
                 else:
@@ -97,21 +100,9 @@ class TextCollector():
                     except:
                         content = None
                 break
-            except URLError as e:
+            except Exception as e:
                 counts += 1
-                print('URLError | ', url_page, ' | ', e, ' | counts: ', counts)
-                sleep(uniform(
-                    counts * self.min_time_sleep, counts * self.max_time_sleep
-                ))
-            except HTTPError as e:
-                counts += 1
-                print('HTTPError | ', url_page, ' | ', e, ' | counts: ', counts)
-                sleep(uniform(
-                    counts * self.min_time_sleep, counts * self.max_time_sleep
-                ))
-            except socket.timeout as e:
-                counts += 1
-                print('socket timeout | ', url_page, ' | ', e, ' | counts: ', counts)
+                print('ERROR request | ', url_page, ' | ', e, ' | counts: ', counts)
                 sleep(uniform(
                     counts * self.min_time_sleep, counts * self.max_time_sleep
                 ))
@@ -145,32 +136,39 @@ class TextCollector():
         return urls
     
     def text_collect(self, url):
-        html = self.parce_content(url)
-        soup = BeautifulSoup(html, 'html.parser')
-        title = soup.find('meta', property="og:title")["content"]
-        citation_keywords = soup.find('meta', {'name': 'citation_keywords'})['content']
-        anno = soup.find('meta', property="og:description")["content"]
-        authors = ', '.join(
-            [x['content'] for x in soup.find_all('meta', {'name': 'citation_author'})]
+        html = self.parce_content(
+            url,
+            proxies=self.proxies
         )
-        publication_date = soup.find('meta', {'name': 'citation_publication_date'})['content']
-        citation_journals = ', '.join(
-            [x['content'] for x in soup.find_all('meta', {'name': 'citation_journal_title'})]
-        )
-        citation_keywords =soup.find('meta', {'name': 'citation_keywords'})['content']
-        full_text = soup.find('div', class_='ocr').text
-        data = {
-            "data": {
-                "title": title,
-                "authors": authors,
-                "publication_date": publication_date,
-                "citation_journals": citation_journals,
-                "keywords": citation_keywords,
-                "anno": anno,
-                "url": url,
-                "text": full_text
+        try:
+            soup = BeautifulSoup(html, 'html.parser')
+            title = soup.find('meta', property="og:title")["content"]
+            citation_keywords = soup.find('meta', {'name': 'citation_keywords'})['content']
+            anno = soup.find('meta', property="og:description")["content"]
+            authors = ', '.join(
+                [x['content'] for x in soup.find_all('meta', {'name': 'citation_author'})]
+            )
+            publication_date = soup.find('meta', {'name': 'citation_publication_date'})['content']
+            citation_journals = ', '.join(
+                [x['content'] for x in soup.find_all('meta', {'name': 'citation_journal_title'})]
+            )
+            citation_keywords =soup.find('meta', {'name': 'citation_keywords'})['content']
+            full_text = soup.find('div', class_='ocr').text
+            data = {
+                "data": {
+                    "title": title,
+                    "authors": authors,
+                    "publication_date": publication_date,
+                    "citation_journals": citation_journals,
+                    "keywords": citation_keywords,
+                    "anno": anno,
+                    "url": url,
+                    "text": full_text
+                }
             }
-        }
+        except Exception as e:
+            print('ERROR parce | ', url)
+            data = {}
         return data
     
     def s3_write(self, data, file_path, local=False):
@@ -188,15 +186,29 @@ class TextCollector():
         except Exception as e:
             return e
     
-    def s3_write_many(self, urls, prefix, start_num, len_count=5, local=False):
+    def s3_write_many(self, urls, local=False):
         try:
+            log_file = 'history.json'
             for i, url in enumerate(tqdm(urls)):
-                data = self.text_collect(url)
-                num_str = '0' * (len_count - len(str(start_num + i))) + str(start_num + i)
-                file_path = f'{prefix}_{num_str}.json'
-                self.s3_write(data, file_path)
+                if os.path.isfile(log_file):
+                    with open(log_file) as file:
+                        logs = json.load(file)
+                    collected_urls = [list(x.keys())[0] for x in logs]
+                else:
+                    logs, collected_urls = [], []
+                if url not in collected_urls:
+                    data = self.text_collect(url)
+                    if data:
+                        file_path = f'{hex(getrandbits(128))}.json'
+                        self.s3_write(data, file_path)
+                        logs.append({url: file_path})
+                        with open(log_file, 'w') as file:
+                            json.dump(logs, file)
+                    else:
+                        print('no data to save for url:', url)
+                else:
+                    print('already loaded:', url)
                 sleep(uniform(self.min_time_sleep, self.max_time_sleep))
-                print('')
             return True
         except Exception as e:
             return e
@@ -212,17 +224,20 @@ def main():
         :prefix: prefix for files to be saved into s3 bucket
         :start_num: number to be inserted to start file name
         
-     example: python parce_texts.py https://cyberleninka.ru search.txt sgd_article 7
+     example: python parce_texts.py https://<some_site_name>.ru proxies.json search.txt
     
     """
     base_url = sys.argv[1]
-    search_file = sys.argv[2]
-    prefix = sys.argv[3]
-    start_num = int(sys.argv[4])
+    proxies_file = sys.argv[2]
+    search_file = sys.argv[3]
+    
+    with open('proxies.json') as file:
+        PROXIES = json.load(file)
     
     collector = TextCollector(
         base_url=base_url,
         user_agent=USER_AGENT, 
+        proxies = PROXIES,
         timeout=TIMEOUT, 
         max_counts=MAX_COUNTS,
         min_time_sleep=MIN_TIME_SLEEP, 
@@ -231,11 +246,7 @@ def main():
     )
     lines = collector.search_words(search_file)
     urls = collector.urls_collect(lines, max_arts=MAX_ARTS)
-    result = collector.s3_write_many(
-        urls,
-        prefix=prefix, 
-        start_num=start_num
-    )
+    result = collector.s3_write_many(urls)
     if result == True:
         print('finished')
     else: 
